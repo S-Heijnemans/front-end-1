706355294354d29da690210b5b312ce99d3e7249
S-Heijnemans <153827872+S-Heijnemans@users.noreply.github.com>
2025-03-13 12:36:45 +0100

    make a loading for the API

--- react-build-periode3/src/components/api/PokeApiList.tsx
+++ react-build-periode3/src/components/api/PokeApiList.tsx
@@ -1,25 +1,39 @@
 import { useEffect, useState } from "react";
 import fetchPoke, { Poke } from "./PokeApi";
 
+
 function PokeApiList() {
     const [pokemon, setPokemon] = useState<Poke[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
 
     useEffect(() => {
-        fetchPoke().then((data) => setPokemon(data));
+        setLoading(true);
+        fetchPoke().then((data) => {
+            setPokemon(data);
+            setLoading(false);
+        });
     }, []);
 
     return(
-        <>
-            {pokemon.map((poke) => (
-                <div >
-                    <h1>{poke.name}</h1>
-                    <p>{poke.id}</p>
-                    <p>Height: {poke.height}</p>
-                    <p>Weight: {poke.weight}</p>
-                    <img src={poke.sprites.front_default} alt={`${poke.name} image`}/>
-                </div>
-            ))}
-        </>
+        <div>
+            {loading ?(
+                <p>Loading...</p>
+            ) : (
+                <>
+                {pokemon.map((poke, index) => (
+                    <div key={index}>
+                        <h1>{poke.name}</h1>
+                        <p>{poke.id}</p>
+                        <p>Height: {poke.height}</p>
+                        <p>Weight: {poke.weight}</p>
+                        <img src={poke.sprites.front_default} alt={`${poke.name} image`}/>
+                    </div>
+                ))}
+                </>
+            )}
+            
+        </div>
     )
 }
 
