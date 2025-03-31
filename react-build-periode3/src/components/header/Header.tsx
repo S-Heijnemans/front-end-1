4786d9a2711d7d47069afaded7f887cb82afcbf6
S-Heijnemans <153827872+S-Heijnemans@users.noreply.github.com>
2025-03-31 11:17:39 +0200

    Api item styling

--- react-build-periode3/src/components/header/Header.tsx
+++ react-build-periode3/src/components/header/Header.tsx
@@ -1,13 +1,14 @@
-import {ButtonRounded } from "../buttons/Buttons"
+import { BrowserRouter, Routes } from "react-router-dom"
+import { ButtonRounded } from "../buttons/Buttons"
 
 function Header() {
     return(
-        <div
-        className="flex flex-row gap-4 p-2 justify-end border-b-[1px] border-b-white border-solid"
-        >
-            <ButtonRounded 
-            lable="Login"/>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <ButtonRounded lable=""/>
+                    
+            </Routes>
+        </BrowserRouter>
     )
 }
 
